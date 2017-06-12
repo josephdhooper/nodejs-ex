d4f5aca19d48552ddf65bfda837ff6d1985b79a3
Your Name Here <jdhooper@gmail.com>
2017-06-11 23:38:06 -0400

    more updates

--- server.js
+++ server.js
@@ -3,19 +3,7 @@ var express = require('express'),
     fs      = require('fs'),
     app     = express(),
     eps     = require('ejs'),
-    path = require('path'),
-
-    MongoClient = require('mongodb').MongoClient,
-    Server = require('mongodb').Server,
-
     morgan  = require('morgan');
-
-
-var app = express();
-// app.set('port', process.env.PORT || 3000); 
-app.set('views', path.join(__dirname, 'views'));
-app.use(express.json());
-app.use(express.urlencoded()); 
     
 Object.assign=require('object-assign')
 
@@ -66,134 +54,42 @@ var initDb = function(callback) {
     dbDetails.url = mongoURLLabel;
     dbDetails.type = 'MongoDB';
 
-    fileDriver = new FileDriver(db); //<--
-    collectionDriver = new CollectionDriver(db);
-
     console.log('Connected to MongoDB at: %s', mongoURL);
   });
 };
 
-app.use(express.static(path.join(__dirname, 'views')));
-
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
+  // try to initialize the db on every request if it's not already
+  // initialized.
+  if (!db) {
+    initDb(function(err){});
+  }
+  if (db) {
+    var col = db.collection('counts');
+    // Create a document with request IP and current time of request
+    col.insert({ip: req.ip, date: Date.now()});
+    col.count(function(err, count){
+      res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
+    });
+  } else {
+    res.render('index.html', { pageCountMessage : null});
+  }
 });
 
-app.post('/files', function(req,res) {fileDriver.handleUploadRequest(req,res);});
-app.get('/files/:id', function(req, res) {fileDriver.handleGet(req,res);}); 
-
-app.get('/:collection', function(req, res, next) {  
-   var params = req.params;
-   var query = req.query.query; //1
-   if (query) {
-        query = JSON.parse(query); //2
-        collectionDriver.query(req.params.collection, query, returnCollectionResults(req,res)); //3
-   } else {
-        collectionDriver.findAll(req.params.collection, returnCollectionResults(req,res)); //4
-   }
+app.get('/pagecount', function (req, res) {
+  // try to initialize the db on every request if it's not already
+  // initialized.
+  if (!db) {
+    initDb(function(err){});
+  }
+  if (db) {
+    db.collection('counts').count(function(err, count ){
+      res.send('{ pageCount: ' + count + '}');
+    });
+  } else {
+    res.send('{ pageCount: -1 }');
+  }
 });
- 
-function returnCollectionResults(req, res) {
-    return function(error, objs) { //5
-        if (error) { res.send(400, error); }
-          else { 
-                    if (req.accepts('html')) { //6
-                        res.render('data',{objects: objs, collection: req.params.collection});
-                    } else {
-                        res.set('Content-Type','application/json');
-                        res.send(200, objs);
-                }
-        }
-    }
-}
- 
-app.get('/:collection/:entity', function(req, res) { //I
-   var params = req.params;
-   var entity = params.entity;
-   var collection = params.collection;
-   if (entity) {
-       collectionDriver.get(collection, entity, function(error, objs) { //J
-          if (error) { res.send(400, error); }
-          else { res.send(200, objs); } //K
-       });
-   } else {
-      res.send(400, {error: 'bad url', url: req.url});
-   }
-});
-
-app.post('/:collection', function(req, res) { //A
-    var object = req.body;
-    var collection = req.params.collection;
-    collectionDriver.save(collection, object, function(err,docs) {
-          if (err) { res.send(400, err); } 
-          else { res.send(201, docs); } //B
-     });
-});
-
-app.put('/:collection/:entity', function(req, res) { //A
-    var params = req.params;
-    var entity = params.entity;
-    var collection = params.collection;
-    if (entity) {
-       collectionDriver.update(collection, req.body, entity, function(error, objs) { //B
-          if (error) { res.send(400, error); }
-          else { res.send(200, objs); } //C
-       });
-   } else {
-     var error = { "message" : "Cannot PUT a whole collection" }
-     res.send(400, error);
-   }
-});
-
-app.delete('/:collection/:entity', function(req, res) { //A
-    var params = req.params;
-    var entity = params.entity;
-    var collection = params.collection;
-    if (entity) {
-       collectionDriver.delete(collection, entity, function(error, objs) { //B
-          if (error) { res.send(400, error); }
-          else { res.send(200, objs); } //C 200 b/c includes the original doc
-       });
-   } else {
-       var error = { "message" : "Cannot DELETE a whole collection" }
-       res.send(400, error);
-   }
-});
-
-// app.get('/', function (req, res) {
-//   // try to initialize the db on every request if it's not already
-//   // initialized.
-//   if (!db) {
-//     // initDb(function(err){});
-//     console.error("Error! Exiting... Must start MongoDB first");
-//       process.exit(1);
-//   }
-//   if (db) {
-//     var col = db.collection('counts');
-//     // Create a document with request IP and current time of request
-//     col.insert({ip: req.ip, date: Date.now()});
-//     col.count(function(err, count){
-//       res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
-//     });
-//   } else {
-//     res.render('index.html', { pageCountMessage : null});
-//   }
-// });
-
-// app.get('/pagecount', function (req, res) {
-//   // try to initialize the db on every request if it's not already
-//   // initialized.
-//   if (!db) {
-//     initDb(function(err){});
-//   }
-//   if (db) {
-//     db.collection('counts').count(function(err, count ){
-//       res.send('{ pageCount: ' + count + '}');
-//     });
-//   } else {
-//     res.send('{ pageCount: -1 }');
-//   }
-// });
 
 // error handling
 app.use(function(err, req, res, next){
